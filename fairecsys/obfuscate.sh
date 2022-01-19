67da9632d392376268c39d3b842a3a38aa983866
Mauro Marini <mauro.marini97@gmail.com>
2022-01-19 11:15:26 +0100

    update fairrecsys

--- fairecsys/obfuscate.sh
+++ fairecsys/obfuscate.sh
@@ -1,10 +1,8 @@
 DIR="./fairrecsys/"
 if [ ! -d "$DIR" ]; then
-  git clone https://github.com/edizel/fairrecsys.git
+  git clone https://github.com/marinimau/fairrecsys.git
 fi
 
 source ../venv/bin/activate
 
-# python3 fairrecsys/src/alg_distMet_mu1.py ../../altered_m1 ../../input/users.csv ../../input/recs.csv 20 0.2
-
-python3 alg_distMet_mu1.py altered_fair_matrix ../data/ml-1m-users.csv ../data/ml-1m-pred-wrmf.csv 20 0.2
+python3 fairrecsys/src/alg_distMet_mu1.py output/altered.csv input/users.csv input/recs.csv 20 0.2
