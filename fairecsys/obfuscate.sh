ea5c83899271e35e3b59cb7032136dff155adcb0
Mauro Marini <mauro.marini97@gmail.com>
2022-01-19 11:55:46 +0100

    update fairrecsys script

--- fairecsys/obfuscate.sh
+++ fairecsys/obfuscate.sh
@@ -5,4 +5,17 @@ fi
 
 source ../venv/bin/activate
 
-python3 fairrecsys/src/alg_distMet_mu1.py output/altered.csv input/users.csv input/recs.csv 20 0.2
+classLabels=("gender" "age")
+cutOffs=(10 20 50)
+models=("user-knn" "bprmf" "wrmf" "multidae")
+
+for m in "${models[@]}"
+do
+  for c in "${cutOffs[@]}"
+  do
+    for g in "${classLabels[@]}";
+    do
+      python3 fairrecsys/src/alg_distMet_mu1.py output/altered_"$m"_"$g"_cutoff_"$c".csv input/users_"$g".csv input/recs.csv "$c" 0.2
+    done
+  done
+done
